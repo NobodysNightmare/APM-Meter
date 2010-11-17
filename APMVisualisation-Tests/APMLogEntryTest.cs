aa1d1e736e42e26e427c776c04ed7388c85d20b6
Jan Sandbrink <fischikowski@web.de>
2010-11-17 12:39:15 +0100

    started adding tests (very basic at the moment)

---
+++ APMVisualisation-Tests/APMLogEntryTest.cs
@@ -0,0 +1,78 @@
+﻿using APMVisualisation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace APMVisualisation_Tests
+{
+    [TestClass()]
+    public class APMLogEntryTest
+    {
+
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        [TestMethod()]
+        public void APMLogEntryConstructorTest()
+        {
+            byte[] buffer = { (byte)1, (byte)0, (byte)0, (byte)0, (byte)2, (byte)0, (byte)0, (byte)0, (byte)3, (byte)0, (byte)0, (byte)0 };
+            int offset = 0;
+            APMLogEntry target = new APMLogEntry(buffer, offset);
+            Assert.AreEqual(1, target.actions);
+            Assert.AreEqual(2, target.time);
+            Assert.AreEqual(3, target.apm);
+        }
+
+        [TestMethod()]
+        public void APMLogEntryConstructorTestWithOffset()
+        {
+            byte[] buffer = { (byte)1, (byte)0, (byte)0, (byte)0, (byte)2, (byte)0, (byte)0, (byte)0, (byte)3, (byte)0, (byte)0, (byte)0, (byte)4, (byte)0, (byte)0, (byte)0 };
+            int offset = 4;
+            APMLogEntry target = new APMLogEntry(buffer, offset);
+            Assert.AreEqual(2, target.actions);
+            Assert.AreEqual(3, target.time);
+            Assert.AreEqual(4, target.apm);
+        }
+    }
+}
--- APMVisualisation-Tests/Properties/AssemblyInfo.cs
+++ APMVisualisation-Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("APMVisualisation")]
+[assembly: AssemblyTitle("APMVisualisation-Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("APMVisualisation")]
+[assembly: AssemblyProduct("APMVisualisation-Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e6e2ec92-b7f9-4320-a94d-322ca4d06bef")]
+[assembly: Guid("8191c745-1727-4f5f-8c0b-24dc41123c06")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("APMVisualisation-Tests")]
