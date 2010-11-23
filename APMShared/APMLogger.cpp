90836673ecfd4b4d24b3d78ae119da09eb14c041
Jan Sandbrink <fischikowski@web.de>
2010-11-23 20:59:51 +0100

    added Head to log-file (writing side)

--- APMShared/APMLogger.cpp
+++ APMShared/APMLogger.cpp
@@ -4,7 +4,32 @@
 APMLogger::APMLogger(LPCTSTR filename) {
 	hFile = CreateFile(filename, GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	if(hFile == INVALID_HANDLE_VALUE)
-		printf("");
+		printf("\r\nCould not open log-file\r\n");
+	else {
+		//set basic header info
+		APMLogHeader* head = new APMLogHeader();
+		head->file_id[0]	= 'A';
+		head->file_id[1]	= 'P';
+		head->file_id[2]	= 'M';
+		head->version		= (char)1;
+		head->header_size = sizeof(APMLogHeader);
+
+		//set time-info
+		SYSTEMTIME now;
+		GetLocalTime(&now);
+		head->year = now.wYear;
+		head->month = now.wMonth;
+		head->day = now.wDay;
+		head->hour = now.wHour;
+		head->minute = now.wMinute;
+
+		//write the header
+		DWORD written = 0;
+		//WriteFile(hFile, head, sizeof(APMLogHeader), &written, NULL);
+		//TODO write converter and reader first
+
+		delete head;
+	}
 }
 
 APMLogger::~APMLogger() {
