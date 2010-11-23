bb25ad966037415438c59647ad95e2eaa6bd5a7f
Jan Sandbrink <fischikowski@web.de>
2010-11-23 21:16:39 +0100

    APMLogData.cs is now head-aware (and ignores the head currently)

--- APMVisualisation/APMLogData.cs
+++ APMVisualisation/APMLogData.cs
@@ -53,26 +53,45 @@ namespace APMVisualisation
         public APMLogData(String filename)
         {
             entries = new List<APMLogEntry>();
-            readEntries(filename);
+            FileStream input = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            //read head
+            byte[] pre_buffer = new byte[8];
+            readAtLeast(input, pre_buffer, 8);
+            //TODO check for "APM" and version
+            Int32 head_len = BitConverter.ToInt32(pre_buffer, 4);
+
+            readHeader(input, 1, head_len);
+            readEntries(input);
+
             total_time = new TimeSpan(0, 0, 0, 0, entries[entries.Count - 1].time);
         }
 
-        private void readEntries(String filename)
+        private void readHeader(FileStream input, byte version, int length)
         {
-            FileStream input = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+            input.Position = length;
+        }
+
+        private void readEntries(FileStream input)
+        {
             byte[] buffer = new byte[APMLogEntry.entry_size * max_read_blocks];
 
             while (input.Position < input.Length)
             {
-                int read_len = 0;
-                do
-                {
-                    read_len += input.Read(buffer, read_len, buffer.Length - read_len);
-                } while (read_len < APMLogEntry.entry_size);
-
+                int read_len = readAtLeast(input, buffer, APMLogEntry.entry_size);
                 for (int i = 0; i < read_len; i += APMLogEntry.entry_size)
                     entries.Add(new APMLogEntry(buffer, i));
             }
+        }
+
+        private int readAtLeast(FileStream input, byte[] buffer, int min_len)
+        {
+            int read_len = 0;
+            do
+            {
+                read_len += input.Read(buffer, read_len, buffer.Length - read_len);
+            } while (read_len < min_len);
+            return read_len;
         }
     }
 }
